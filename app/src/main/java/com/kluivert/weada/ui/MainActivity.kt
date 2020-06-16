054467a513808dffb95f480145d4544943433c96
kluivertcode <tristankluivert@gmail.com>
2020-06-16 22:44:56 +0100

    not much

--- app/src/main/java/com/kluivert/weada/ui/MainActivity.kt
+++ app/src/main/java/com/kluivert/weada/ui/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.kluivert.weada
+package com.kluivert.weada.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kluivert.weada.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
